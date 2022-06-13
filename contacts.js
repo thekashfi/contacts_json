1d77a888ebf0b53093b86f6a433fc07efc5bdaad
thekashfi <alikashfi12345@gmail.com>
2022-06-13 10:13:52 +0430

    where it all begins

---
+++ contacts.js
@@ -0,0 +1,14 @@
+function contacts () {
+    return {
+        contacts: [
+            {"name": "John", "mail": "example@gmail.com", "phone": "09171234567", "relation": "friend"},
+            {"name": "Dave", "mail": "dave@gmail.com", "phone": "09171234567", "relation": "brother"},
+            {"name": "Jane", "mail": "theJane@gmail.com", "phone": "09171234567", "relation": "sister"},
+            {"name": "Joe", "mail": "goodJoe@gmail.com", "phone": "09171234567", "relation": "friend"},
+            {"name": "Ross", "mail": "RossGeller@gmail.com", "phone": "09171234567", "relation": "friend"},
+        ]
+    }
+}
+// window.onload = () => {
+//
+// }
